210a503361823d1282d86e779a7f0aa71c35a7cd
GentillePlume <melvin.lemoine@feather-project.org>
2018-10-10 10:36:02 +0200

    Changed the background-color menu selector

--- src/js/nav-selector.js
+++ src/js/nav-selector.js
@@ -3,16 +3,15 @@ console.log("nav-selector.js launched");
 function nav_selector(pagename) {
   switch (pagename) {
     case "dashboard":
-      $(".nav__menu--dashboard")[0].style.backgroundColor = "red";
+      $(".nav__menu--dashboard")[0].style.backgroundColor = "grey";
       break;
     case "articles":
-      $(".nav__menu--articles")[0].style.backgroundColor = "red";
+      $(".nav__menu--articles")[0].style.backgroundColor = "grey";
       break;
     case "categories":
-      $(".nav__menu--articles")[0].style.backgroundColor = "red";
+      $(".nav__menu--articles")[0].style.backgroundColor = "grey";
       break;
     default:
       alert("%cnav-selector ERROR", "background-color:red;color:white");
-
   }
 }
